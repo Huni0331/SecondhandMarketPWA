f7fb16e6c16c1ede48a9e6c2e8f7efb8d0e1ae9c
Jeong ByeongHun <wjdqudgns4@naver.com>
2023-04-14 20:50:36 +0900

    test

--- src/main/java/com/secondhandmarket/secondhandmarket/SecondhandMarketApplication.java
+++ src/main/java/com/secondhandmarket/secondhandmarket/SecondhandMarketApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondhandMarketApplication { //asdf
+public class SecondhandMarketApplication { //asdfgu
     public static void main(String[] args) {
         SpringApplication.run(SecondhandMarketApplication.class, args);
     }
