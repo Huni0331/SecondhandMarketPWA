f44c6abced3cc87694ff8738b5c0b01c1dd00980
Jeong ByeongHun <wjdqudgns4@naver.com>
2023-04-14 15:16:29 +0900

    Test

---
+++ src/test/java/com/secondhandmarket/secondhandmarket/SecondhandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondhandmarket.secondhandmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
